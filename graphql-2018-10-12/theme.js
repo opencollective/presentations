12934b7550ef0687d4606c6f0052db49a346f800
François Hodierne <francois@hodierne.net>
2018-10-15 12:26:36 +0200

    new: GraphQL presentation

---
+++ graphql-2018-10-12/theme.js
@@ -0,0 +1,10 @@
+// example theme.js
+import { future } from 'mdx-deck/themes';
+import okaidia from 'react-syntax-highlighter/styles/prism/okaidia';
+
+export default {
+  ...future,
+  prism: {
+    style: okaidia,
+  },
+};
